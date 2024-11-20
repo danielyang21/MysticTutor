6872ad395043b490489774fc067a5065a629b904
danielyang21 <danielyanxiaoyang21@gmail.com>
2024-11-20 16:05:39 -0500

    adding card search page

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+
+@app.route('/card-search')
+def card_search():
+    return render_template('card-search.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
