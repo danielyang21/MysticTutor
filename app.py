b1fb8e934a230f6867ebec89f6b61e4050ef5075
danielyang21 <danielyanxiaoyang21@gmail.com>
2024-11-27 11:46:30 -0500

    Search filters are almost working

--- app.py
+++ app.py
@@ -23,21 +23,31 @@ def card_urls():
 
 @app.route('/api/search', methods=['GET'])
 def search():
-    search_term = request.args.get('term')  # Get the search term from the query parameters
-    if not search_term:
-        return jsonify({"error": "No search term provided"}), 400
+    search_term = request.args.get('term')
+    colors = request.args.get('colors')
+    keywords = request.args.get('keywords')
+    types = request.args.get('types')
+    subtypes = request.args.get('subtypes')
+    sets = request.args.get('sets')
+    """ if not search_term:
+        return jsonify({"error": "No search term provided"}), 400 """
     
-    results = search_database(search_term) 
+    if colors: colors = colors.split(',')
+    if keywords: keywords = keywords.split(',')
+    if types: types = types.split(',')
+    if subtypes: subtypes = subtypes.split(',')
+    if sets: sets = sets.split(',')
 
-    return jsonify(results)
 
-@app.route('/api/filter', methods=['GET'])
-def filter():
-    search_term = request.args.get('term')  # Get the search term from the query parameters
-    if not search_term:
-        return jsonify({"error": "No search term provided"}), 400
-    
-    results = search_database(search_term) 
+    print(colors)
+    print(keywords)
+    print(types)
+    print(subtypes)
+    print(sets)
+
+
+
+    results = search_database(search_term,colors,keywords,types,subtypes,sets) 
 
     return jsonify(results)
 
@@ -45,26 +55,66 @@ def filter():
 #HELPER FUNCTIONS (sql)
 
 
-def search_database(search_term):
+def search_database(search_term,colors,keywords,types,subtypes,sets):
     connection = sqlite3.connect('reliquary_tower.db')
     cursor = connection.cursor()
 
-    search_term = f"%{search_term}%"
+    query = '''
+        SELECT DISTINCT cs.image_url
+        FROM CARD_SETS cs
+    '''
+    conditions = []
+    parameters = []
 
-    print(search_term)
+    # Add search term condition
+    if search_term:
+        conditions.append("cs.card_name LIKE ?")
+        parameters.append(f"%{search_term}%")
 
-    # Query the database for card names that match the search term, use the LIKE function to give all results which include the search term
-    cursor.execute('''
-        SELECT image_url
-        FROM CARD_SETS
-        WHERE card_name LIKE ?
-    ''', (search_term,))
-    
+    # Add colors filter
+    if colors:
+        query += " JOIN CARD_COLOURS cc ON cs.card_name = cc.card_name"
+        placeholders = ", ".join("?" for _ in colors)
+        conditions.append(f"cc.colour_name IN ({placeholders})")
+        parameters.extend(colors)
+
+    # Add keywords filter
+    if keywords:
+        query += " JOIN CARD_KEYWORDS ck ON cs.card_name = ck.card_name"
+        conditions.append("ck.keyword_name IN ({})".format(", ".join("?" for _ in keywords)))
+        parameters.extend(keywords)
+
+    # Add types filter
+    if types:
+        query += " JOIN CARD_TYPES ct ON cs.card_name = ct.card_name"
+        conditions.append("ct.type_name IN ({})".format(", ".join("?" for _ in types)))
+        parameters.extend(types)
+
+    # Add subtypes filter
+    if subtypes:
+        query += " JOIN CARD_SUBTYPES cst ON cs.card_name = cst.card_name"
+        conditions.append("cst.subtype_name IN ({})".format(", ".join("?" for _ in subtypes)))
+        parameters.extend(subtypes)
+
+    # Add sets filter
+    if sets:
+        conditions.append("cs.set_code IN ({})".format(", ".join("?" for _ in sets)))
+        parameters.extend(sets)
+
+    # Combine all conditions
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+
+    # Execute the query
+    cursor.execute(query, parameters)
+    print(query)
+    print(parameters)
     results = cursor.fetchall()
 
     # Close the connection
     connection.close()
     return results
+
 
 
 
